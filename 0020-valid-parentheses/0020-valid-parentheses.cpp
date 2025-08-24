db2a81a1560a829210f93309e70ad18bc0c1941d
AAYUSH RAJ <140076026+Aayushraj14@users.noreply.github.com>
2025-08-24 11:10:38 +0530

    Time: 0 ms (100%), Space: 8.9 MB (33.27%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -4,20 +4,19 @@ public:
         stack<char>st;
         for(int i=0;i<s.length();i++){
             char ch=s[i];
-            if(ch=='(' || ch=='{' || ch=='[')st.push(ch);
+            if(st.empty() && (s[i]==')' || s[i]=='}' || s[i]==']'))return false;
+            else if(ch=='(' || ch=='{' || ch=='['){
+                st.push(ch);
+            }
             else{
-                if(!st.empty()){
-                    if(st.top()=='(' && ch==')')st.pop();
+                if(st.top()=='(' && ch==')')st.pop();
                 else if(st.top()=='{' && ch=='}')st.pop();
                 else if(st.top()=='[' && ch==']')st.pop();
                 else{
                     return false;
                 }
-                }else return false;
-                
             }
         }
-        if(st.empty())return true;
-        return false;
+        return (st.empty())?true:false;
     }
 };
