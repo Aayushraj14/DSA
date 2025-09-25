31c8af7f6934ba12a28a8e622b5290bda8e7537c
AAYUSH RAJ <140076026+Aayushraj14@users.noreply.github.com>
2025-09-25 22:12:08 +0530

    Time: 1817 ms (7.91%), Space: 0B (100%) - LeetHub

---
+++ 1268-market-analysis-i/1268-market-analysis-i.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select u.user_id as buyer_id,u.join_date, sum(case when year(o.order_date)=2019 then 1 else 0 end) as orders_in_2019
+from Users u 
+left join 
+Orders o 
+on o.buyer_id=u.user_id
+
+group by u.user_id;
