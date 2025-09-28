cab3f6d4e035015cdafb0430ca4be8412f912445
AAYUSH RAJ <140076026+Aayushraj14@users.noreply.github.com>
2025-09-28 12:14:40 +0530

    Time: 0 ms (100%), Space: 12.8 MB (47.66%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -15,10 +15,15 @@ public:
         if(p==NULL && q==NULL)return true;
         if(p==NULL && q!=NULL)return false;
         if(p!=NULL && q==NULL)return false;
-        if(p->val!=q->val)return false;
-
-        bool ans1=isSameTree(p->left,q->left);
+       
+        if(p->val==q->val){
+             bool ans1=isSameTree(p->left,q->left);
         bool ans2=isSameTree(p->right,q->right);
-        return ans1 && ans2;
+
+        return (ans1&&ans2);
+        
+        }
+        return false;
+       
     }
 };
