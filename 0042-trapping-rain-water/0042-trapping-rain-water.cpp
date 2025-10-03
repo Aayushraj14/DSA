afda6e9159173c99f3027db97e71782bcd9bd012
AAYUSH RAJ <140076026+Aayushraj14@users.noreply.github.com>
2025-10-03 12:55:29 +0530

    Time: 0 ms (100%), Space: 27.3 MB (19.64%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        vector<int>leftheight(height.size());
-       
-        leftheight[0]=height[0];
-        for(int i=1;i<height.size();i++){
-           leftheight[i]=max(leftheight[i-1],height[i]);
+        vector<int>PrevGE(height.size());
+        vector<int>nextGE(height.size());
+
+        int prev=0;
+        int next=0;
+
+        for(int i=0;i<height.size();i++){
+           
+            if(height[i]>prev)prev=height[i];
+             PrevGE[i]=prev;
         }
 
-        vector<int>rightheight(height.size());
-       rightheight[height.size()-1]=height[height.size()-1];
-        for(int i=height.size()-2;i>=0;--i){
-           rightheight[i]=max(rightheight[i+1],height[i]);
+        for(int i=height.size()-1;i>=0;i--){
+            
+            if(height[i]>next)next=height[i];
+            nextGE[i]=next;
         }
-        int water=0;
+
+       int ans=0;
         for(int i=1;i<height.size()-1;i++){
-            water+=(min(leftheight[i],rightheight[i])-height[i]);
+             ans+=(min(PrevGE[i],nextGE[i])-height[i]);
+             
+           
         }
-        return water;
-
+        return ans;
     }
 };
