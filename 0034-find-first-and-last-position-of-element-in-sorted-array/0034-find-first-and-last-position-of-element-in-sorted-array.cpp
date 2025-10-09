8ff135b4bba24a14c4f550282ffbc7d2301595b4
AAYUSH RAJ <140076026+Aayushraj14@users.noreply.github.com>
2025-10-09 17:14:48 +0530

    Time: 0 ms (100%), Space: 17.6 MB (52.64%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
-    int lower(vector<int>&nums,int target){
+    int firstOccurence(vector<int>& nums, int target){
         int start=0;
         int end=nums.size()-1;
         int ans=-1;
         while(start<=end){
-            int mid=(start+end)/2;
+           int mid = start + (end - start) / 2;
+
             if(nums[mid]==target){
                 ans=mid;
                 end=mid-1;
             }
             else if(nums[mid]>target)end=mid-1;
-            else{
-                start=mid+1;
-            }
+            else start=mid+1;
         }
         return ans;
     }
-    int upper(vector<int>&nums,int target){
+
+    int lastOccurence(vector<int>& nums, int target){
         int start=0;
         int end=nums.size()-1;
         int ans=-1;
         while(start<=end){
-            int mid=(start+end)/2;
+           int mid = start + (end - start) / 2;
+
             if(nums[mid]==target){
                 ans=mid;
                 start=mid+1;
@@ -33,13 +34,9 @@ public:
         return ans;
     }
     vector<int> searchRange(vector<int>& nums, int target) {
-        if(nums.size()==0)return {-1,-1};
-        int lowerBound=lower(nums,target);
-        int upperBound=upper(nums,target);
-        vector<int>ans;
-        ans.push_back(lowerBound);
-        ans.push_back(upperBound);
+        int ans1=firstOccurence(nums,target);
+        int ans2=lastOccurence(nums,target);
+        vector<int>ans={ans1,ans2};
         return ans;
-
     }
 };
