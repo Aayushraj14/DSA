043eb690324e2fca8bacde9bcad3608fb2a27257
AAYUSH RAJ <140076026+Aayushraj14@users.noreply.github.com>
2025-10-09 15:28:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Knapsack with Duplicate Items/knapsack-with-duplicate-items.cpp
+++ Difficulty: Medium/Knapsack with Duplicate Items/knapsack-with-duplicate-items.cpp
@@ -2,25 +2,21 @@
 
 class Solution {
   public:
-    int solve(vector<int>& val, vector<int>& wt, int capacity, vector<vector<int>>&dp,int i){
+    int solve(vector<int>& val, vector<int>& wt, int capacity,int i,vector<vector<int>>&dp){
+        if(capacity==0)return 0;
         if(capacity<0)return INT_MIN;
-        if(i>=wt.size() || capacity==0)return 0;
+        if  (i==val.size())return 0;
         
         if(dp[i][capacity]!=-1)return dp[i][capacity];
-        //ussi ko call karo
-        int op1=val[i] + solve(val,wt,capacity-wt[i],dp,i);
-        //include
-        int op2=val[i]+solve(val,wt,capacity-wt[i],dp,i+1);
-        //exclude
-        int op3=solve(val,wt,capacity,dp,i+1);
+        int inc=val[i]+solve(val,wt,capacity-wt[i],i,dp);
+        int exc=solve(val,wt,capacity,i+1,dp);
         
-        return dp[i][capacity]=max(op1,max(op2,op3));
-        
+        return dp[i][capacity]=max({inc,exc});
     }
     int knapSack(vector<int>& val, vector<int>& wt, int capacity) {
         // code here
-        vector<vector<int>>dp(wt.size(),vector<int>(capacity+1,-1));
-        int i=0;
-        return solve(val,wt,capacity,dp,i);
+        
+        vector<vector<int>>dp(val.size()+1,vector<int>(capacity+1,-1));
+        return solve(val,wt,capacity,0,dp);
     }
 };
