7b404fe772a83d27676c23723df98afe5a5d6d37
AAYUSH RAJ <140076026+Aayushraj14@users.noreply.github.com>
2025-10-29 12:25:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rod Cutting/rod-cutting.cpp
+++ Difficulty: Medium/Rod Cutting/rod-cutting.cpp
@@ -2,25 +2,23 @@
 
 class Solution {
   public:
-    int solve(vector<int> &price,int i,int len,vector<vector<int>>&dp){
-        if(len<0)return INT_MIN;
-        if(len==0)return 0;
-        if(i==price.size())return 0;
+    int solve(vector<int> &price,int i,int len, vector<vector<int>>&dp){
         
-        
+        if(i>=price.size() || len<=0)return 0;
+       
         if(dp[i][len]!=-1)return dp[i][len];
-        int incR=price[i]+solve(price,i,len-(i+1),dp);
-        //int inc=price[i]+solve(price,i+1,len-(i+1));
-        int exc=solve(price,i+1,len,dp);
+        int inc=0,exc=0;
+       if(len-(i+1)>=0)
+         inc=price[i]+solve(price,i,len-i-1,dp);
+         exc=solve(price,i+1,len,dp);
+         
+         return dp[i][len]=max(inc,exc);
         
-        return dp[i][len]=max({exc,incR});
     }
     int cutRod(vector<int> &price) {
         // code here
-        
-       
-        int n=price.size();
-         vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        return solve(price,0,n,dp);
+        int len=price.size();
+        vector<vector<int>>dp(price.size(),vector<int>(price.size()+1,-1));
+        return solve(price,0,len,dp);
     }
 };
