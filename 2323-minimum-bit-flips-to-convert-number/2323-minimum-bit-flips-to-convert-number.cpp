cc5aa8de2881c1eea3720b49d4d40ceac635f3b6
AAYUSH RAJ <140076026+Aayushraj14@users.noreply.github.com>
2025-10-31 13:48:28 +0530

    Time: 7 ms (0.3%), Space: 8 MB (60.11%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
@@ -3,14 +3,14 @@ public:
     int minBitFlips(int start, int goal) {
         int count=0;
         while(start || goal){
-            if((start & 1) !=(goal & 1)){
+            if((start & 1)!=(goal&1)){
                 count++;
-                start=start>>1;goal=goal>>1;
             }
-            else{
-                start=start>>1;goal=goal>>1;
-            }
+            start=start>>1;
+            goal=goal>>1;
+            cout<<count<<" ";
         }
+       
         return count;
     }
 };
