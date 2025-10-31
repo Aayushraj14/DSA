2087ea2e677f3f94080d3b1296cb4f8a8b111b7c
AAYUSH RAJ <140076026+Aayushraj14@users.noreply.github.com>
2025-10-31 13:11:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Number of 1 Bits/number-of-1-bits.cpp
+++ Difficulty: Easy/Number of 1 Bits/number-of-1-bits.cpp
@@ -4,7 +4,7 @@ class Solution {
         // Write Your Code here
         int count=0;
         while(n){
-            if(n & 1==1)count++;
+            if((n&1)==1)count++;
             n=n>>1;
         }
         return count;
