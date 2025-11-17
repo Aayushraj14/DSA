c15a4cf238b0283fe9570a561716147c2112739e
AAYUSH RAJ <140076026+Aayushraj14@users.noreply.github.com>
2025-11-17 10:44:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
@@ -1,21 +1,21 @@
 class Solution {
   public:
-    int solve(vector<int>&val,vector<int> &wt,int i,int W,vector<vector<int>>&dp){
-        if(W<0 || i>=val.size())return 0;
+    int solve(int W, vector<int> &val, vector<int> &wt,int i, vector<vector<int>>&dp){
+        if(i>=wt.size())return 0;
+        if(W<=0)return 0;
         
         if(dp[W][i]!=-1)return dp[W][i];
         int inc=0,exc=0;
-        if(W-wt[i]>=0){
-            inc=val[i]+solve(val,wt,i+1,W-wt[i],dp);
+        if(W>=wt[i]){
+            inc=val[i]+solve(W-wt[i],val,wt,i+1,dp);
         }
-            exc=solve(val,wt,i+1,W,dp);
-        
+        exc=solve(W,val,wt,i+1,dp);
         return dp[W][i]=max(inc,exc);
+        
     }
     int knapsack(int W, vector<int> &val, vector<int> &wt) {
         // code here
-        vector<vector<int>>dp(W+1,vector<int>(val.size()+1,-1));
-        return solve(val,wt,0,W,dp);
-        
+        vector<vector<int>>dp(W+1,vector<int>(wt.size()+1,-1));
+        return solve(W,val,wt,0,dp);
     }
 };
